8908b46c340f2e6ac4f962814407b5e5c45ce58f
urish <>
2002-03-19 10:21:47 +0000

    1. Added unload proc. 2. Icon frame now docks upper to chatting line, instead of opening as new window.

--- plus/src/icons/main.tcl
+++ plus/src/icons/main.tcl
@@ -1,42 +1,31 @@
 EXTENTION icons VERSION 1.1 BUILD 1
-# $Id: main.tcl,v 1.1 2002-03-16 10:54:26 urish Exp $
+# $Id: main.tcl,v 1.2 2002-03-19 10:21:47 urish Exp $
 
 <@INCLUDE icons.tcl>
 
 proc init {} {
-    hook	user_image	[namespace current]::user_image
+    hook user_image	[namespace current]::user_image
     bind .inputbox <Control-Key-i>	[namespace current]::ctrl_i_pressed
-}
-
-proc ctrl_i_pressed {} {
-    global inputbox
-    set count 0
-    foreach i [split $inputbox ""] {
-	if {$i == "\005"} {
-	    incr count
-	}
-    }
-    insert_char \005
-    if {$count % 2} {
-	catch {
-	    destroy .icon_select
-	}
-	return
-    }
-    catch {
-	toplevel .icon_select
-    }
-    raise .icon_select
-    focus .inputbox
+    frame .icon_select -background [.inputbox cget -background] -relief raised -width 2
     set j 0
     foreach i [image names] {
 	set lname .icon_select.l$j
-	label $lname -image $i
-	bind $lname <1> [list insert_char [namespace tail $i]\005]
-	bind $lname <1> "+destroy .icon_select"
-	pack $lname
+	label $lname -image $i -background [.icon_select cget -background]
+	bind $lname <1> [list [namespace current]::insert_icon [namespace tail $i]]
+	pack $lname -side left
 	incr j
     }
+    grid .icon_select -in . -row 180 -column 1 -sticky nesw
+}
+
+proc unload {} {
+    unhook user_image	[namespace current]::user_image
+    bind .inputbox <Control-Key-i>	""
+    destroy .icon_select
+}
+
+proc ctrl_i_pressed {} {
+    insert_char \005
 }
 
 proc image_exists {img} {
@@ -46,11 +35,23 @@ proc image_exists {img} {
 proc user_image {names} {
     set result ""
     foreach name [split $names :] {
+	if [string match {[123456789]\**} $name] {
+	    set count [string index $name 0]
+	    set name [string range $name 2 end]
+	} else {
+	    set count 1
+	}
 	set name [namespace current]::icons::[string tolower [string trim $name]]
 	if ![image_exists $name] {
 	    continue
 	}
-	lappend result $name
+	for {set i 0} {$i < $count} {incr i} {
+	    lappend result $name
+	}
     }
     return $result
+}
+
+proc insert_icon {name} {
+    insert_char \005$name\005
 }
